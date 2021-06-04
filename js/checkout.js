992ef69cf6dbabeeffbe8106892329ad3bb70b4e
Mikhael Jonathan <mikhael629@gmail.com>
2021-06-04 20:53:20 +0700

    edit amount

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,5 @@
 let curLocalStorage = localStorage.getItem('CART');
+let totalPrice = 0;
 
 if (curLocalStorage == undefined) {
     document.getElementById('carts').insertAdjacentHTML('afterbegin',
@@ -16,7 +17,6 @@ if (curLocalStorage == undefined) {
     $('#total').css('display', 'none');
 
 } else {
-    let totalPrice = 0;
     curLocalStorage = JSON.parse(curLocalStorage);
 
     for (let i = 0; i < curLocalStorage.length; i++){
@@ -27,15 +27,23 @@ if (curLocalStorage == undefined) {
         document.getElementById('carts').insertAdjacentHTML('beforeend',
         `
         <div class="cart">
+            <div class="cart-overlay">
+                <p>Edit Amount</p>
+                <div class="edit-amount">
+                    <button class="amount-button" onclick="decrementAmount(${i})">-</button>
+                    <p class="amount" id="amount-${i}">${cart.stock}</p>
+                    <button class="amount-button" onclick="incrementAmount(${i})">+</button>
+                </div>
+            </div>
             <img src="../src/${cart.type === 'bread' ? `breads/bread${cart.id}.jpg` : 'banners/custom-cake-banner.jpg'}" alt="Cart ${i}" />
             <div class="cart-info">
                 <h3>${cart.name}</h3>
                 ${cart.type === 'bread' ? '' : `<p>${cart.message}</p>`}
-                <p><span class="bold">Subtotal:</span> Rp ${subTotal},00</p>
+                <p id="subtotal-${i}"><span class="bold">Subtotal:</span> Rp ${subTotal},00</p>
             </div>
             <div class="quantity">
                 <div class="circle">
-                    <p>${cart.stock}</p>
+                    <p id="cart-stock-${i}">${cart.stock}</p>
                 </div>
             </div>
         </div>
@@ -59,3 +67,29 @@ $('#yes-clear-cart').click(function(){
     localStorage.clear();
     window.location.reload();
 });
+
+function decrementAmount(idx){
+    let amount = Number($(`#amount-${idx}`).html());
+    amount--;
+
+    let curPrice = JSON.parse(JSON.parse(localStorage.getItem('CART'))[idx]).price;
+    totalPrice -= curPrice;
+
+    $(`#amount-${idx}`).html(amount);
+    $(`#cart-stock-${idx}`).html(amount);
+    $(`#subtotal-${idx}`).html(`<span class="bold">Subtotal:</span> Rp ${curPrice * amount},00`)
+    $('#total').html(`<span>Total:</span> Rp ${totalPrice},00`);
+}
+
+function incrementAmount(idx){
+    let amount = Number($(`#amount-${idx}`).html());
+    amount++;
+    
+    let curPrice = JSON.parse(JSON.parse(localStorage.getItem('CART'))[idx]).price;
+    totalPrice += curPrice;
+
+    $(`#amount-${idx}`).html(amount);
+    $(`#cart-stock-${idx}`).html(amount);
+    $(`#subtotal-${idx}`).html(`<span class="bold">Subtotal:</span> Rp ${curPrice * amount},00`)
+    $('#total').html(`<span>Total:</span> Rp ${totalPrice},00`);
+}
