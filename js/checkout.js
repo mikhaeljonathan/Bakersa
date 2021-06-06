6d7a0d6f49eb93346225a70a6704eff17fb7c697
Mikhael Jonathan <mikhael629@gmail.com>
2021-06-06 20:34:32 +0700

    delete cart

--- js/checkout.js
+++ js/checkout.js
@@ -2,9 +2,9 @@ let curLocalStorage = localStorage.getItem('CART');
 curLocalStorage = JSON.parse(curLocalStorage);
 let totalPrice = 0;
 
-if (curLocalStorage == undefined) {
+if (curLocalStorage == undefined || curLocalStorage.length === 0) {
     document.getElementById('carts').insertAdjacentHTML('afterbegin',
-    `
+        `
     <div class="empty-cart">
         <div class="overlay">
             <p>Your cart is empty :(</p>
@@ -18,14 +18,14 @@ if (curLocalStorage == undefined) {
     $('#total').css('display', 'none');
 
 } else {
-    
-    for (let i = 0; i < curLocalStorage.length; i++){
+
+    for (let i = 0; i < curLocalStorage.length; i++) {
         let cart = JSON.parse(curLocalStorage[i]);
         let subTotal = cart.stock * cart.price;
         totalPrice += subTotal;
 
         document.getElementById('carts').insertAdjacentHTML('beforeend',
-        `
+            `
         <div class="cart">
             <div class="cart-overlay">
                 <p>Edit Amount</p>
@@ -54,23 +54,40 @@ if (curLocalStorage == undefined) {
     $('#total').html(`<span>Total:</span> Rp ${totalPrice},00`);
 }
 
-$('#clear-cart').click(function(){
-    $('.overlay-dialog').css('visibility', 'visible');
-    $('#dialog-message').html(`⚠️ Are you sure want to clear this cart?`);
-})
-
-$('#cancel-clear-cart').click(function(){
-    $('.overlay-dialog').css('visibility', 'hidden');
+$('.overlay-dialog').click(function (e) {
+    let classTarget = e.target.classList[0];
+    if (classTarget === 'overlay-dialog') {
+        $('.overlay-dialog').css('visibility', 'hidden');
+        $('#yes-clear-cart').css('display', 'none');
+        $('#yes-delete-item').css('display', 'none');
+    }
 });
 
-$('#yes-clear-cart').click(function(){
+$('#clear-cart').click(function () {
+    $('.overlay-dialog').css('visibility', 'visible');
+    $('#yes-clear-cart').css('display', 'inline-block');
+    $('#dialog-message').html(`⚠️ Are you sure want to <strong>clear this cart</strong>?`);
+})
+
+$('#cancel').click(function () {
+    $('.overlay-dialog').css('visibility', 'hidden');
+    $('#yes-clear-cart').css('display', 'none');
+    $('#yes-delete-item').css('display', 'none');
+});
+
+$('#yes-clear-cart').click(function () {
     localStorage.clear();
     window.location.reload();
 });
 
-function decrementAmount(idx){
+function decrementAmount(idx) {
     let amount = Number($(`#amount-${idx}`).html());
     amount--;
+
+    if (amount === 0) {
+        handleZeroAmount(idx);
+        return;
+    }
 
     let curPrice = JSON.parse(JSON.parse(localStorage.getItem('CART'))[idx]).price;
     totalPrice -= curPrice;
@@ -83,10 +100,10 @@ function decrementAmount(idx){
     updateAmount(amount, idx);
 }
 
-function incrementAmount(idx){
+function incrementAmount(idx) {
     let amount = Number($(`#amount-${idx}`).html());
     amount++;
-    
+
     let curPrice = JSON.parse(JSON.parse(localStorage.getItem('CART'))[idx]).price;
     totalPrice += curPrice;
 
@@ -107,3 +124,18 @@ function updateAmount(amount, idx) {
 
     localStorage.setItem('CART', JSON.stringify(curLocalStorage));
 }
+
+let soonToBeDeletedItemIdx = -1;
+function handleZeroAmount(idx) {
+    $('.overlay-dialog').css('visibility', 'visible');
+    $('#yes-delete-item').css('display', 'inline-block');
+    $('#dialog-message').html(`⚠️ Are you sure want to delete item: <strong>${JSON.parse(curLocalStorage[idx]).name}</strong> from your cart?`);
+    soonToBeDeletedItemIdx = idx;
+}
+
+$('#yes-delete-item').click(function () {
+    curLocalStorage.splice(soonToBeDeletedItemIdx, 1);
+    localStorage.setItem('CART', JSON.stringify(curLocalStorage));
+
+    window.location.reload();
+});
